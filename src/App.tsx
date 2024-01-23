58dc7ea208d5612a07a885bd33b5a985296da6c9
Igor Klein <igorklein1988@gmail.com>
2024-01-23 18:46:28 +0100

    add product page

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,9 @@ import RegisterPage from './components/LoginRegisterForgotPassword/RegisterPage'
 import RestorePassword from './components/LoginRegisterForgotPassword/RestorePassword';
 import RestorePasswordWait from './components/LoginRegisterForgotPassword/RestoreUserValidationWait';
 import RestoreEnterNewPAssword from './components/LoginRegisterForgotPassword/RestoreEnterNewPAssword';
+import Auction from './components/Auction';
+import AddProductPage from './features/products/AddProduct/AddProduct';
+import AddedSuccessPage from './features/products/AddedSuccessPage/AddedSuccessPage';
 
 function App(): JSX.Element {
 	return (
@@ -32,7 +35,10 @@ function App(): JSX.Element {
 						<Route path="change_password" element={<ChangePassword />} />
 						<Route path="my_auctions" element={<MyAuctions />} />
 					</Route>
-					<Route path="product/details/" element={<ProductDetails />} />
+					<Route path="products/details" element={<ProductDetails />} />
+					<Route path="products/add_product" element={<AddProductPage />} />
+					<Route path="products/product_added_success" element={<AddedSuccessPage />} />
+
 					<Route path="shipping" element={<Shipping />} />
 					<Route path="payment" element={<Payment />} />
 					<Route path="return" element={<Return />} />
@@ -42,6 +48,7 @@ function App(): JSX.Element {
 					<Route path="restore_password" element={<RestorePassword />} />
 					<Route path="restore_password_wait" element={<RestorePasswordWait />} />
 					<Route path="restore_password_new" element={<RestoreEnterNewPAssword />} />
+					<Route path="aa" element={<Auction />} />
 					<Route path="*" element={<NotFound />} />
 				</Route>
 			</Routes>
